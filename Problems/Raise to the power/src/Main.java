6ce12b3b55af9f4e293e01a18562f322521071c0
MadhurToppo <madhur.toppo@gmail.com>
2020-10-10 07:34:16 +0530

    Raise to the power

---
+++ Problems/Raise to the power/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static long power(int n, int m) {
+    // write your code here
+    long result = 1;
+    for (int i = 0; i < m; i++) {
+      result *= n;
+    }
+    return result;
+  }
+
+  /* Do not change code below */
+  public static void main(String[] args) {
+    final Scanner scanner = new Scanner(System.in);
+    final int n = scanner.nextInt();
+    final int m = scanner.nextInt();
+    System.out.println(power(n, m));
+  }
+}
